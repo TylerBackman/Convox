a9ae0e4b802678441f01a505ee46f0aa87728e8c
TylerBackman <nospam@nospam.nospam>
2013-04-20 09:26:39 -0700

    trying to fix maven deps issues

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "A Functional cheminformatics library for Clojure/Incanter"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.openscience.cdk/cdk-io "1.4.16"]
-                 [org.openscience.cdk/cdk-libiocml "1.4.16"]
+                 [org.openscience.cdk/cdk-ioformats "1.4.16"]
                  [org.openscience.cdk/cdk-smiles "1.4.16"]]
   :repositories [["ambit.uni-plovdiv.bg" "http://ambit.uni-plovdiv.bg:8083/nexus/content/repositories/thirdparty/"]])
